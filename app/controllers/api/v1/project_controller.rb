b156def055b876f673add6d58b05e4887d6c6bf5
Nilesh Pandey <nlhpandey@gmail.com>
2016-05-09 12:08:11 -0400

    prevent duplicate user_projects

--- app/controllers/api/v1/project_controller.rb
+++ app/controllers/api/v1/project_controller.rb
@@ -28,7 +28,7 @@ class Api::V1::ProjectController < Api::V1::BaseController
     pp user = User.find_by_id(params[:user_id])
     pp project = Project.find_by_access_code(params[:access_code])
 
-    if user && project
+    if user && project && !user.projects.find_by_access_code(params[:access_code])
       user.projects << project
     end
 
